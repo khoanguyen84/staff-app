f51724bd3192a739092fb00bfbba91711c74dba4
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-09-27 17:06:38 +0700

    fix

--- src/services/FileUploadService.js
+++ src/services/FileUploadService.js
@@ -12,3 +12,14 @@ class FileUploadService{
 
 export default FileUploadService;
 
+
+
+// const timestamp = new Date().getTime()
+// const string = `public_id=${<Add public_id>}&timestamp=${timestamp}<add your api secret>`
+// const signature = await sha1(string)
+// const formData = new FormData()
+// formData.append("public_id",<public_id>)
+// formData.append("signature",signature)
+// formData.append("api_key",<your api_key>)
+// formData.append("timestamp",timestamp)
+// const res = await ax.post("https://api.cloudinary.com/v1_1/<your cloud name>/image/destroy", formData) 
