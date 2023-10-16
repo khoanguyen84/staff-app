732cf0836845864d05eaac912fec090825ebe024
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-10-16 14:48:22 +0700

    bug fix after delete staff

--- src/components/staffs/StaffList/StaffList.js
+++ src/components/staffs/StaffList/StaffList.js
@@ -28,34 +28,37 @@ function StaffList() {
         } catch (error) {
             setState({ ...state, errorMessage: error.message });
         }
-    }, [])
-    const handleDelete = function(staff){
+    }, [,])
+    const handleDelete = function (staff) {
         let confirmed = window.confirm("Are you sure to remove this staff?");
-        if(confirmed){
+        if (confirmed) {
             try {
                 setState({ ...state, loading: true });
-                async function deleteData(){
+                async function deleteData() {
                     let resStaff = await StaffService.deleteStaff(staff.id);
                     let filename = staff.avatar.split('/').pop().split('.')[0];
-                    let resDeleteImage = await FileUploadService.destroyImage(filename);
-                    if(resStaff.data){
-                        navigate("/staff-app", {replace : true});
-                    }
+                    let resDeleteImage = FileUploadService.destroyImage(filename);
+                    let resStaffs = await StaffService.getStaffs();
+                    setState({
+                        ...state,
+                        staffs: resStaffs.data,
+                        loading: false
+                    });
                 }
                 deleteData()
-                
+
             } catch (error) {
                 setState({ ...state, loading: false, errorMessage: error.message });
             }
         }
     }
-    const handleSearch = async function(event){
-        setState({...state, loading: true});
+    const handleSearch = async function (event) {
+        setState({ ...state, loading: true });
         let resStaffs = await StaffService.getStaffs();
         setState({
             ...state,
             staffs: resStaffs.data.filter(staff => staff.name.toLowerCase().includes(event.target.value.toLowerCase())),
-            loading : false
+            loading: false
         })
     }
     const { loading, staffs, errorMessage } = state;
@@ -64,7 +67,7 @@ function StaffList() {
             <section className="staff-info my-3">
                 <div className="container">
                     <div className="d-flex align-items-center">
-                        <h4>Staff Manager</h4> 
+                        <h4>Staff Manager</h4>
                         <Link to={"/staff-app/staff/add"} className="btn btn-primary btn-sm ms-2"><i className="fa fa-plus-circle me-1"></i>New</Link>
                     </div>
                     <p className="fst-italic">Deserunt ex minim ipsum consectetur incididunt nostrud qui sunt qui. Deserunt reprehenderit quis esse dolore officia elit sit. Ad deserunt voluptate quis consectetur aliqua proident cupidatat exercitation aliqua consequat occaecat. Labore ex occaecat enim elit sit qui velit id. Qui ut sit anim excepteur ullamco ipsum. Consequat aliqua incididunt anim ex dolore.</p>
@@ -98,7 +101,7 @@ function StaffList() {
                                                         <div className="d-flex flex-column align-items-center">
                                                             <Link to={`/staff-app/staff/view/${staff.id}`} className="btn btn-warning btn-sm mb-1"><i className="fa fa-eye"></i></Link>
                                                             <Link to={`/staff-app/staff/edit/${staff.id}`} className="btn btn-primary btn-sm mb-1"><i className="fa fa-edit"></i></Link>
-                                                            <button onClick={()=> handleDelete(staff)} className="btn btn-danger btn-sm"><i className="fa fa-trash"></i></button>
+                                                            <button onClick={() => handleDelete(staff)} className="btn btn-danger btn-sm"><i className="fa fa-trash"></i></button>
                                                         </div>
                                                     </div>
                                                 </div>
