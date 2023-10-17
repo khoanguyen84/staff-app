429ebbba26d243ce0e0a84391c6abfeaa45f509a
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-10-17 10:53:19 +0700

    change cloudinary account

--- src/services/FileUploadService.js
+++ src/services/FileUploadService.js
@@ -2,25 +2,25 @@ import axios from 'axios';
 import { CommonService } from "./CommonService";
 import sha1 from 'sha1';
 
-class FileUploadService{
-    static uploadImage(imageFile){
+class FileUploadService {
+    static uploadImage(imageFile) {
         const formData = new FormData();
         formData.append("file", imageFile);
-        formData.append("upload_preset", "a09ikbyc");
+        formData.append("upload_preset", "lhih0wco");
         return axios.post(CommonService.CLOUDINARY_UPLOAD_API_URL, formData);
     }
 
-    static destroyImage(filename){
+    static destroyImage(filename) {
         const timestamp = new Date().getTime();
         const public_id = filename;
-        const api_key = "338315595645698";
-        const api_secret_key = "ymunfk97k5CSvTXtHkL5PSTWCJ4";
+        const api_key = "469739551169485"; //"338315595645698";
+        const api_secret_key = "INfCfBAJudCmSPTv8Hb_Exh6uOM"; //"ymunfk97k5CSvTXtHkL5PSTWCJ4";
         const shaString = `public_id=${public_id}&timestamp=${timestamp}${api_secret_key}`;
         const signature = sha1(shaString)
         const formData = new FormData();
         formData.append("public_id", public_id);
         formData.append("signature", signature);
-        formData.append("api_key",api_key);
+        formData.append("api_key", api_key);
         formData.append("timestamp", timestamp);
         return axios.post(CommonService.CLOUDINARY_DESTROY_API_URL, formData);
     }
